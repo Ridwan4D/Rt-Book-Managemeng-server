267978402247df6916a8d431acdfd63d38cd56cb
Ridwan4D <ridwanul161@gmail.com>
2024-05-15 14:46:20 +0600

    token varify add in borrowd book and all book

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', "https://rt-library-management-118f5.web.app", "https://rt-library-management.web.app", "https://rt-library-management-118f5.firebaseapp.com"],
   credentials: true,
 }));
 app.use(express.json())
@@ -29,6 +29,11 @@ const client = new MongoClient(uri, {
 
 
 // ================================================== middle ware start ==================================================
+const logger = (req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+}
+
 const verifyToken = (req, res, next) => {
   const token = req?.cookies?.token;
   // this is for no token (no token are found)
@@ -45,21 +50,26 @@ const verifyToken = (req, res, next) => {
   })
 }
 // ================================================== middle ware end ==================================================
-
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const booksCollection = client.db("libraryBooksdb").collection("books");
     const borrowCollection = client.db("libraryBooksdb").collection("borrowBooks");
     const categoryCollection = client.db("libraryBooksdb").collection("category");
 
     // authorization related apis
-    app.post("/jwt", async (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      console.log("user for token", user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" })
       res
         .cookie('token', token, {
           httpOnly: true,
@@ -68,13 +78,14 @@ async function run() {
         })
         .send({ success: true })
     })
+
     app.post('/logout', async (req, res) => {
-      const user = req.body;
-      console.log("logging out user: ", user);
+      const user = req.body
       res
-        .clearCookie('token', { maxAge: 0 })
-        .send({ success: true })
+      .clearCookie('token',{maxAge: 0})
+      .send({ success: true })
     })
+
 
     // category related apis
     app.get("/allCategories", async (req, res) => {
@@ -93,7 +104,7 @@ async function run() {
 
 
     // all book related apis
-    app.get("/allBooks", async (req, res) => {
+    app.get("/allBooks",logger,verifyToken, async (req, res) => {
       const result = await booksCollection.find().toArray()
       res.send(result)
     })
@@ -107,7 +118,7 @@ async function run() {
     })
 
 
-    app.post("/addBooks",verifyToken, async (req, res) => {
+    app.post("/addBooks",logger, async (req, res) => {
       const bookInfo = req.body;
       const result = await booksCollection.insertOne(bookInfo);
       res.send(result);
@@ -146,8 +157,9 @@ async function run() {
 
 
     // borrow book related apis
-    app.get("/allBorrowBooks/:email", async (req, res) => {
+    app.get("/allBorrowBooks/:email",logger,verifyToken, async (req, res) => {
       const query = { email: req.params.email };
+      console.log("tok tok token: ",req.cookies);
       const result = await borrowCollection.find(query).toArray();
       res.send(result)
     })
@@ -166,7 +178,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
